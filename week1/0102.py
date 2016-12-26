6604243ef847952ef0f795f31f4cf1a4025a2194
Wenny Hidayat <wenny.h@live.com>
2016-12-26 18:43:26 +1100

    Added plotting

--- week1/0102.py
+++ week1/0102.py
@@ -2,9 +2,11 @@
 READING DATA FROM CERTAIN RANGES ONLY
 READING DATA FROM MULTIPLE FILES AND AGGREGATE
 BUILDING, JOINING DATAFRAMES
+PLOTTING MULTIPLE DATA
 """
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def create_empty_dataframe_index_dates():
     start_date = '2010-01-22'
@@ -23,11 +25,22 @@ def add_data(dataframe, base, symbol):
     df = df.rename(columns = {'Adj Close' : symbol})
 #    return dataframe.join(df).dropna()
     return dataframe.join(df, how='inner')
+ 
+def plot_data(df, title="Stock prices"):
+    '''Plot stock prices'''
+    ax = df.plot(title=title)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
+    
+def normalize_data(df):
+    return df / df.ix[0, :]
     
 def run_this():
     foo = create_empty_dataframe_index_dates()
     for sym in ['AAPL', 'IBM', 'SPY']:
         foo = add_data(foo, "../data", sym)
+        
     # by row
     #print foo.ix['2010-01-01' : '2010-01-31']
     
@@ -37,7 +50,10 @@ def run_this():
     
     # by row and col
     print foo.ix['2010-01-01':'2010-01-31', ['SPY', 'IBM']]
-    
+    plot_data(foo)
+
+    # Normalization
+    plot_data(normalize_data(foo))
     
 
 """
