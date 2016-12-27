fd1440c3a26ef6c8570bbef640948fc2f356be89
Wenny Hidayat <wenny.h@live.com>
2016-12-27 22:31:23 +1100

    Beginning 0107

---
+++ week1/0107.py
@@ -0,0 +1,5 @@
+"""
+Daily returns, on its own, are not very useful.
+They are useful when compared to other stocks' returns, since it indicates if the phenomenon is
+ across the market or exclusive to that particular stock and hence, its performance.
+"""
