13988d9707d9ae34b25cb2e6fa488581d062d0d5
Wenny Hidayat <wenny.h@live.com>
2016-12-28 18:52:45 +1100

    Generated sample graph to show function graphically

--- week1/0108_optimizer.py
+++ week1/0108_optimizer.py
@@ -18,5 +18,13 @@ def run_this():
     print("Minima at:")
     print("X = {}, Y = {}".format(min_result.x, min_result.fun))
 
+    Xplot = np.linspace(0.5, 2.5, 21)
+    Yplot = f(Xplot)
+    plt.plot(Xplot, Yplot)
+
+    plt.plot(min_result.x, min_result.fun, 'ro')
+    plt.title("Minima of an objective function")
+    plt.show()
+
 if __name__ == "__main__":
     run_this()
