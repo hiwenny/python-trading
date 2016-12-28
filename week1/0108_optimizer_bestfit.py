cfc959c498fd27fc2c3f9e1a08e46ab463abd86e
Wenny Hidayat <wenny.h@live.com>
2016-12-28 20:15:19 +1100

    Poly minimizer

--- week1/0108_optimizer_bestfit.py
+++ week1/0108_optimizer_bestfit.py
@@ -1,5 +1,6 @@
 """
 Used to find min value for function
+Will also work for polys, awesome!
 """
 
 import pandas as pd
@@ -14,8 +15,21 @@ def error(line, data):
     err = np.sum( (data[:, 1] - (line[0] * data[:, 0] + line[1]) ) ** 2)
     return err
 
-def run_this():
-    # Define original line
+def fit_line(data, error_func):
+    # Generate initial guess for line model
+    l = np.float32([0, np.mean(data[:, 1])]) # slope=0, intercept=mean(y values)
+
+    # Plot initial guess
+    x_ends = np.float32([-5, 5])
+    plt.plot(x_ends, l[0] * x_ends + l[1], 'm--', linewidth=2.0, label="Initial guess")
+
+    # Call optimizer to minimize error function
+    # args=(data,) is not typo, it is so that we can pass args to it - see docs.
+    result = spo.minimize(error_func, l, args=(data,), method='SLSQP', options={'disp': True})
+    return result.x
+
+def run_this_for_linear():
+    # Define original line starting point
     lamb_orig = np.float32([4, 2])
     print("Original line: C0 = {}, C1 = {}".format(lamb_orig[0], lamb_orig[1]))
     X_orig = np.linspace(0, 10, 21)
@@ -29,9 +43,30 @@ def run_this():
     plt.plot(data[:, 0], data[:, 1], 'go', label="Data points")
 
     # Line fitting
-    # line = fit_line(data, error)
-    # print "Fitted lin"
-    # plt.show()
+    line = fit_line(data, error)
+    print ("Fitted line: C0 = {}, C1 = {}".format(line[0], line[1]))
+    plt.plot(data[:, 0], line[0] * data[:, 0] + line[1], 'r--', linewidth=2.0, label="LOBF")
+
+    plt.show()
+
+def error_poly(C, data):
+    err = np.sum((data[:, 1] - np.polyval(C, data[:, 0])) ** 2)
+    return err
+
+def fit_poly(data, error_func, degree=3):
+    # Initial guess
+    Cguess = np.poly1d(np.ones(degree+1, dtype=np.float32))
+
+    # Plot initial guess
+    x = np.linspace(-5, 5, 21)
+    plt.plot(x, np.polyval(Cguess, x), 'm--', linewidth=2.0, label="Initial guess")
+
+    # Call optimizer to min err func
+    res = spo.minimize(error_func, Cguess, args=(data,), method='SLSQP', options={'disp': True})
+    return np.poly1d(res.x)
+
+def run_this():
+
 
 if __name__ == "__main__":
     run_this()
